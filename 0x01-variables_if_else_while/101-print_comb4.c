163a03dcb41538acd2ca1bd338f920b128dabe3b
antenehzewdu <mescrem1@gmail.com>
2022-07-04 05:00:34 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ for (k = j + 1; k < 10; k++)
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
 putchar((k % 10) + '0');
-if (i != 7 && j != 8 && k != 9)
+if (i == 7 && j == 8 && k == 9)
 continue;
 putchar(',');
 putchar(' ');
