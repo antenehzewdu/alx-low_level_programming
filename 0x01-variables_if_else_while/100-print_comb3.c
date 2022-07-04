b88c3666166daac1ff42fee3643f3cacadd7699e
antenehzewdu <mescrem1@gmail.com>
2022-07-04 02:58:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@ for (num2 = num1 + 1; num2 < 10; num2++)
 {
 putchar((num1 % 10) + '0');
 putchar((num2 % 10) + '0');
-if (i == 8 && j == 9)
+if (num1 == 8 && num2 == 9)
 putchar(',');
 putchar(' ');
 }
