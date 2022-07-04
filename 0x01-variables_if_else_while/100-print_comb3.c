7cd7002c2d918609c44f46287d9b7c60881504f1
antenehzewdu <mescrem1@gmail.com>
2022-07-04 03:51:11 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,20 @@ int main(void)
 {
 int i;
 int j;
-for (i = 0; i <= 9; i++)
+for (i = 10; i <= 19; i++)
 {
-for (j = i + 1; j <= 9; j++)
+for (j = i + 1; j <= 19; j++)
+{
+if ((j % 10) > (i % 10))
 {
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
-if (i != 8 && j != 9)
+if (i != 18 && j != 19)
+{
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
