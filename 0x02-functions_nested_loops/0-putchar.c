cdea85004eb2d1007e6950836dcf51ebcca56aa4
antenehzewdu <mescrem1@gmail.com>
2022-07-04 22:13:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - prints _putchar
+*
+* Return:always 0
+*
+*/
+int main(void)
+{
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putChar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
+}
