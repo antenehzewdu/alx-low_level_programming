053cd8f4076e2786bee0d1a6b83a5c79bd82f134
antenehzewdu <mescrem1@gmail.com>
2022-07-06 06:23:43 -0700

    readme added

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,1 @@
-#include "main.h"
-/**
-* main - function print out putchar
-* Return:always 0
-*/
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-return (0);
-}
+int _putchar(char c);
