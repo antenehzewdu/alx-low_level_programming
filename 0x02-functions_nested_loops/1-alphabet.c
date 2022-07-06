0cfeaf7efcaf6b9534cfeeb71372df0445e09708
antenehzewdu <mescrem1@gmail.com>
2022-07-06 10:03:05 -0700

    add function that print alphbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+* print_alphbet - prints the alphabet, in lowercase
+*
+*/
+void print_alphbet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
