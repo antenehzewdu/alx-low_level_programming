ce726e2c0ec7e778d9a24376b2b58879f681a8aa
antenehzewdu <mescrem1@gmail.com>
2022-07-10 03:40:23 -0700

    more number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 
 /**
 * more_number - print more number
-* Return:10 times of the number since 0 up to 14.
 */
 void more_numbers(void)
 {
@@ -12,9 +11,7 @@ for (x = 0; x < 10; x++)
 for (y = 0; y < 15; y++)
 {
 if (y > 9)
-{
 _putchar((y / 10) + '0');
-}
 _putchar((y % 10) + '0');
 }
 _putchar('\n');
