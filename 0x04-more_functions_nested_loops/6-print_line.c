42d2342f29137648c629ddb6b363122a1a974dc8
antenehzewdu <mescrem1@gmail.com>
2022-07-10 03:56:19 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_line - prints line
+* @n: integer parameter
+*/
+void print_line(int n)
+{
+int x;
+if (n <= 0)
+_putchar('\n');
+for (x = 0; x <= n; x++)
+_putchar('\n');
+}
