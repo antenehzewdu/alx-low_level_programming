9c220569f92bf1b486317aab4ddae8753327b4a4
antenehzewdu <mescrem1@gmail.com>
2022-07-11 14:53:32 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _atoi - convert a string to an intiger
+* @s: a pointer to character
+* Return: void
+*/
+int _atoi(char *s)
+{
+int sign;
+unsigned int num;
+char *temp;
+sign = 1;
+num = 0;
+temp = s;
+while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+{
+if (*temp == '-')
+sign *= -1;
+temp++
+}
+if (*temp != '\0')
+{
+do {
+num = num * 10 + (*temp - '0');
+temp++
+}
+while (*temp >= '0' && *temp <= '9')
+}
+return (num *sign);
+}
