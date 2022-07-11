13100d34fb23c0d23f4a83d0e236ff0c562c97af
antenehzewdu <mescrem1@gmail.com>
2022-07-11 15:20:01 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - generate randam valid passward
+* Return: success 0.
+*/
+int main(void)
+{
+int pass, sum;
+srand(time(NULL));
+sum = 0;
+while (sum <= 2645)
+{
+pass = (rand() % 128);
+sum += pass;
+printf("%c", pass);
+}
+printf("%c", 2772 - sum);
+return (0);
+}
