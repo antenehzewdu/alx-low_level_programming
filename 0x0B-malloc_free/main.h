93d6f9c23fa8be1cdc1c3ff21c26fa7fc8705bf4
antenehzewdu <mescrem1@gmail.com>
2022-07-26 09:06:06 -0700

    1-strdup.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,6 @@ char *create_array(unsigned int size, char c);
 int _putchar(char c);
 #include <stdlib.h>
 #include <stddef.h>
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
