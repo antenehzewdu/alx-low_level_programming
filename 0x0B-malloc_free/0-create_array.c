252a259f7ad13a67dfd299c006257167046add77
antenehzewdu <mescrem1@gmail.com>
2022-07-26 08:01:00 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,11 +7,11 @@
 */
 char *create_array(unsigned int size, char c)
 {
- unsigned int i;
+unsigned int i;
 char *s;
 if (size == 0)
 return (NULL);
-s = (char*)malloc(size * sizeof(char));
+s = (char *)malloc(size * sizeof(char));
 if (s == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
