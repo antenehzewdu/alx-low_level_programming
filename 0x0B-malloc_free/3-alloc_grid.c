8c5042e1b62cfca42346cfec70059845d63c82dc
antenehzewdu <mescrem1@gmail.com>
2022-07-27 12:23:47 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,12 +13,13 @@ if (width <= 0 && height <= 0)
 return (NULL);
 a = malloc(sizeof(int *) * height);
 if (a == NULL)
+{
 free(a);
 return (NULL);
+}
 for  (i = 0; i < height; i++)
 {
 a[i] = malloc(sizeof(int *) * width);
-}
 if (a[i] == NULL)
 {
 for (j = i; j > 0; j--)
